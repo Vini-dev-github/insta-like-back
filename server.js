8fdfdd22cf7c8a6868ef0d4b444d46fc8c55ae43
unknown <viniciuslima.profissional@gmail.com>
2024-11-21 20:22:36 -0300

    create function search id

--- server.js
+++ server.js
@@ -2,26 +2,32 @@ import express from "express"
 
 const posts = [
     {
+        id:1,
         descricao: "Foto de teste",
         imagem: "https://placecats.com/millie/300/150"
       },
       {
+        id:2,
         descricao: "Gato brincando com um novelo de lã",
         imagem: "https://placekitten.com/400/300"
       },
       {
+        id:3,
         descricao: "Gatinho dormindo em uma caixa",
         imagem: "https://placekitten.com/200/200"
       },
       {
+        id:4,
         descricao: "Gatos olhando pela janela",
         imagem: "https://placekitten.com/300/200"
       },
       {
+        id:5,
         descricao: "Gato curioso cheirando uma planta",
         imagem: "https://placekitten.com/250/250"
       },
       {
+        id:6,
         descricao: "Gatos se aconchegando em um cobertor",
         imagem: "https://placekitten.com/400/400"
       },
@@ -36,5 +42,16 @@ app.listen(3000, () =>{
 });
 
 app.get("/posts", (req, res) =>{
-    res.status(200).send("Hell-o world");
+    res.status(200).json(posts);
 });
+
+function buscarPostsPorId(id){
+    return posts.findIndex((posts) =>{
+        return posts.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostsPorId(req.params.id)
+    res.status(200).json(posts[index])
+})
